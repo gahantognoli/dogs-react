1b912862cd6e3131e87bb973daa55fbe030205db
Gabriel Antognoli <gabriel_antognoli@hotmail.com>
2020-10-03 11:58:01 -0300

    Criação dos comentários nas fotos

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -1,31 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../../UserContext';
+import Image from '../Helper/Image';
 import PhotoComments from './PhotoComments';
 import styles from './PhotoContent.module.css';
+import PhotoDelete from './PhotoDelete';
 
 const PhotoContent = ({ data }) => {
+  const user = React.useContext(UserContext);
+  console.log(user);
+  console.log(data);
+
   const { photo, comments } = data;
+
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
-        <img src={photo.src} alt={photo.title} />
+        <Image src={photo.src} alt={photo.title} />
       </div>
       <div className={styles.details}>
-          <div>
-            <p className={styles.author}>
+        <div>
+          <p className={styles.author}>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
               <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
-              <span className={styles.visualizacoes}>{photo.acessos}</span>
-            </p>
-            <h1 className="title">
-              <Link to={`/photo/${photo.id}`}>{photo.title}</Link>
-            </h1>
-            <ul className={styles.attributes}>
-              <li>{photo.peso} kg</li>
-              <li>{photo.idade} anos</li>
-            </ul>
-            <PhotoComments id={photo.id} comments={comments} />
-          </div>
+            )}
+            <span className={styles.visualizacoes}>{photo.acessos}</span>
+          </p>
+          <h1 className="title">
+            <Link to={`/photo/${photo.id}`}>{photo.title}</Link>
+          </h1>
+          <ul className={styles.attributes}>
+            <li>{photo.peso} kg</li>
+            <li>{photo.idade} anos</li>
+          </ul>
         </div>
+      </div>
+      <PhotoComments id={photo.id} comments={comments} />
     </div>
   );
 };
